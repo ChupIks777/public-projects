6a41672eea4d8ce79040e5fdba351c41680d57e0
ChupIks777 <145923594+ChupIks777@users.noreply.github.com>
2025-03-10 00:50:59 +0300

    Update numbers_to_assembler.py

--- numbers_to_assembler.py
+++ numbers_to_assembler.py
@@ -1,3 +1,6 @@
+class MyError(Exception):
+    pass
+
 def print_res(result):
     for i in result:
         print(i)
@@ -38,55 +41,95 @@ def code_to_command(code, index):
     adressed_commands_dict = dict(zip(keys, values))  # словарь адресных команд
     if code[0] in keys:
         if code[1] in "01234567":
-            return adressed_commands_dict[code[0]] + "$S" + code[1:]
+            return adressed_commands_dict[code[0]] + "0x" + code[1:]
         else:
             return adressed_commands_dict[code[0]] + indirect_addressing_command(code, index)
     else:
         return branch_command(code, index)
 
 result = []
+while True:
+    try:
+        print("Введите номер начальной ячейки:")
+        first_cell_index = input()
+        x = int(first_cell_index, 16)
+        if x < 0 or x > 2**11 - 1:
+            raise MyError("Число не входит в ограничения памяти БЭВМ")
+        break
+    except Exception as e:
+        print(f"Ошибка: {e}")
 
-print("Введите номер начальной ячейки:")
-first_cell_index = input()
 first_cell_index = (3 - len(first_cell_index)) * '0' + first_cell_index
 result.append("ORG 0x" + first_cell_index)
 
 index = int(first_cell_index, 16)
 
 print("Вводите первую часть ячеек с данными (пустая строка = остановка):")
-data_cell = input()
-result.append("S" + hex(index)[2:].upper() + ": WORD 0x" + data_cell)
-index += 1
+data_cell = " "
 while data_cell != '':
     data_cell = input()
     if data_cell != "":
         result.append("S" + hex(index)[2:].upper() + ": WORD 0x" + data_cell)
         index += 1
 
-keys = ["0000", "0100", "0200", "0280", "0300", "0380", "0400", "0480", "0500", "0580", "0600", "0680", "0700", "0740", "0780", "0800", "0900", "0A00", "0B00", "0C00", "0D00", "0E00"]
-values = ["NOP", "HLT", "CLA", "NOT", "CLC", "CMC", "ROL", "ROR", "ASL", "ASR", "SXTB", "SWAB", "INC", "DEC", "NEG", "POP", "POPF", "RET", "IRET", "PUSH", "PUSHF", "SWAP"]
+keys = ["0000", "0100", "0200", "0280", "0300", "0380", "0400", "0480", "0500", "0580",
+        "0600", "0680", "0700", "0740", "0780", "0800", "0900", "0A00", "0B00", "0C00", "0D00", "0E00"] #безадресные команды
+values = ["NOP", "HLT", "CLA", "NOT", "CLC", "CMC", "ROL", "ROR", "ASL", "ASR", "SXTB",
+          "SWAB", "INC", "DEC", "NEG", "POP", "POPF", "RET", "IRET", "PUSH", "PUSHF", "SWAP"] #и их мнемоника
 unadressed_commands_dict = dict(zip(keys, values)) #словарь безадресных команд
 
 print("Теперь вводите ваши команды в 16-ричной системе счисления:")
 while True:
-    input_code = input(); input_code = (4 - len(input_code)) * "0" + input_code
-    if input_code in keys:
-        result.append("S" + hex(index)[2:].upper() + ": " + unadressed_commands_dict[input_code])
-        index += 1
-        if input_code == '0100': break
-    else:
-        result.append("S" + hex(index)[2:].upper() + ": " + code_to_command(input_code, index))
-        index += 1
-print("Теперь вводите оставшиеся данные (пустая строка = остановка):")
-data_cell = input()
-if data_cell == '':
-    print_res(result)
-else:
-    result.append("S"  + hex(index)[2:].upper() + ": WORD 0x" + data_cell)
-    index += 1
-    while data_cell != '':
-        data_cell = input()
-        if data_cell != "":
-            result.append("S" + hex(index)[2:].upper() + ": WORD 0x" + data_cell)
+    try:
+        input_code = input()
+        x = int(input_code, 16)
+        input_code = (4 - len(input_code)) * "0" + input_code
+        if input_code in keys:
+            result.append("S" + hex(index)[2:].upper() + ": " + unadressed_commands_dict[input_code])
             index += 1
+            if input_code == '0100': break
+        else:
+            result.append("S" + hex(index)[2:].upper() + ": " + code_to_command(input_code, index))
+            index += 1
+    except Exception as e:
+        print(f"Ошибка: {e}")
+print("Если у вас есть данные сразу после программы, вписывайте их построчно. Если их нет или они закончились, введите пустую строку:")
+data_cell = " "
+while data_cell != "":
+    data_cell = input()
+    if data_cell != "":
+        result.append("S" + hex(index)[2:].upper() + ": WORD 0x" + data_cell)
+        index += 1
+
+print("Введите номер ячейки в 16 системе счисления, с которой продолжается программа (пустая строка = остановка):")
+flag = True
+while True:
+    try:
+        second_cell_index = input()
+        if second_cell_index == "":
+            flag = False
+            break
+        index = int(second_cell_index, 16)
+        if x < 0 or x > 2**11 - 1:
+            raise MyError("Число не входит в ограничения памяти БЭВМ")
+        break
+    except Exception as e:
+        print(f"Ошибка: {e}")
+result.append("ORG 0x" + second_cell_index)
+if flag:
+    print("Теперь вводите оставшиеся команды (пустая строка = остановка):")
+    while input_code != "":
+        try:
+            input_code = input()
+            x = int(input_code, 16)
+            input_code = (4 - len(input_code)) * "0" + input_code
+            if input_code in keys:
+                result.append("S" + hex(index)[2:].upper() + ": " + unadressed_commands_dict[input_code])
+                index += 1
+                if input_code == '0100': break
+            else:
+                result.append("S" + hex(index)[2:].upper() + ": " + code_to_command(input_code, index))
+                index += 1
+        except Exception as e:
+            print(f"Ошибка: {e}")
 print_res(result)
